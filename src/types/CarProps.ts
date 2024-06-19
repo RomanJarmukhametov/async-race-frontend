58832846097004a37dadee14e4332de9f09a4160
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-19 12:48:25 +0500

    feat: add car props

---
+++ src/types/CarProps.ts
@@ -0,0 +1,10 @@
+/* This TypeScript code defines an interface named `CarProps` that specifies the structure of an object
+representing a car. The interface includes three properties: `id` of type number, `name` of type
+string, and `color` of type string. */
+interface CarProps {
+  id: number;
+  name: string;
+  color: string;
+}
+
+export default CarProps;
