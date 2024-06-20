13ebcc12a9b06e3825620141167252c0f30f2b9f
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-20 12:32:41 +0500

    feat: improve responsiveness of Logo component

--- src/components/custom/Logo.tsx
+++ src/components/custom/Logo.tsx
@@ -1,5 +1,9 @@
 function Logo() {
-  return <p className="text-4xl font-bold text-white">Async Race</p>;
+  return (
+    <p className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-white">
+      Async Race
+    </p>
+  );
 }
 
 export default Logo;
