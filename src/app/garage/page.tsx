2d51db6829da3c4633aed74c92d9755b3c74522b
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-21 11:36:34 +0500

    feat: add loading page

--- src/app/garage/page.tsx
+++ src/app/garage/page.tsx
@@ -31,7 +31,11 @@ function GaragePage() {
   }, []);
 
   if (loading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="fixed inset-0 flex items-center justify-center bg-gray-200 bg-opacity-50">
+        <div className="animate-spin h-12 w-12 border-t-4 border-gray-600 rounded-full" />
+      </div>
+    );
   }
 
   if (error) {
