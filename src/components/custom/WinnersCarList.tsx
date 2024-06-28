6fbdd563a8917e34007ee45db290d27a49ee9e48
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-28 13:27:26 +0500

    feat: add fetching car name and color from garage

--- src/components/custom/WinnersCarList.tsx
+++ src/components/custom/WinnersCarList.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+'use client';
+
+import React, { useState, useEffect } from 'react';
 import {
   Table,
   TableHead,
@@ -7,16 +9,47 @@ import {
   TableCell,
 } from '@/components/custom/Table';
 
+import CarIcon from '@/components/custom/CarIcon';
+
 import WinnerDelete from '@/components/custom/WinnerDelete';
+
+import { getCarById } from '@/lib/api/garage';
 
 import GetWinnersParams from '@/types/GetWinnersParams';
 
 function WinnersCarList({ winners }: { winners: GetWinnersParams[] }) {
+  const [carNames, setCarNames] = useState<{ [key: number]: string }>({});
+  const [carColors, setCarColors] = useState<{ [key: number]: string }>({});
+
+  // Fetch car names and colors from the garage
+  useEffect(() => {
+    const fetchCarNamesAndColors = async () => {
+      const carNamesMap: { [key: number]: string } = {};
+      const carColorsMap: { [key: number]: string } = {};
+      const fetchPromises = winners.map(async (winner) => {
+        try {
+          const car = await getCarById(winner.id);
+          carNamesMap[winner.id] = car.name;
+          carColorsMap[winner.id] = car.color;
+        } catch (error) {
+          carNamesMap[winner.id] = 'Unknown';
+          carColorsMap[winner.id] = 'gray';
+        }
+      });
+      await Promise.all(fetchPromises);
+      setCarNames(carNamesMap);
+      setCarColors(carColorsMap);
+    };
+
+    fetchCarNamesAndColors();
+  }, [winners]);
+
   return (
     <Table>
       <TableHead>
         <TableRow>
           <TableCell isHeader>Car ID</TableCell>
+          <TableCell isHeader>Car Name</TableCell>
           <TableCell isHeader>Total Wins</TableCell>
           <TableCell isHeader>Time</TableCell>
           <TableCell isHeader>Actions</TableCell>
@@ -26,6 +59,10 @@ function WinnersCarList({ winners }: { winners: GetWinnersParams[] }) {
         {winners.map((winner) => (
           <TableRow key={winner.id}>
             <TableCell>{winner.id}</TableCell>
+            <TableCell>
+              <CarIcon color={carColors[winner.id]} />
+              {carNames[winner.id] || 'Loading...'}
+            </TableCell>
             <TableCell>{winner.wins}</TableCell>
             <TableCell>{winner.time}</TableCell>
             <TableCell>
