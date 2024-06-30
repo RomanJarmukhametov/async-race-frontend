256dda3937098c778316dd929f06cba8e1eaf7a0
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-06-30 16:47:09 +0500

    refactor: place getCarsInDriveMode into a separate file

--- src/components/custom/StartRace.tsx
+++ src/components/custom/StartRace.tsx
@@ -8,32 +8,8 @@ import {
   updateWinner,
   getWinnerByCarId,
 } from '@/lib/api/winners';
+import { getCarsInDriveMode } from '@/lib/getCarsInDriveMode';
 import { useRace } from '@/context/RaceContext';
-
-// Function to get cars with driveMode set to true
-function getCarsInDriveMode(): {
-  id: number;
-  time: number;
-}[] {
-  const carsInDriveMode: { id: number; time: number }[] = [];
-
-  // Loop through local storage keys
-  for (let i = 0; i < localStorage.length; i += 1) {
-    const key = localStorage.key(i);
-    if (key && key.endsWith('-driveMode')) {
-      const id = parseInt(key.split('-')[0], 10); // Convert id to number
-      const driveMode = localStorage.getItem(key) === 'true';
-      const timeInSecondsKey = `${id}-timeInSeconds`;
-      const time = parseFloat(localStorage.getItem(timeInSecondsKey) || '0');
-
-      if (driveMode) {
-        carsInDriveMode.push({ id, time });
-      }
-    }
-  }
-
-  return carsInDriveMode;
-}
 
 function StartRace() {
   const [toastMessage, setToastMessage] = useState('');
---
+++ src/lib/getCarsInDriveMode.ts
@@ -0,0 +1,24 @@
+// Function to get cars with driveMode set to true
+export function getCarsInDriveMode(): {
+  id: number;
+  time: number;
+}[] {
+  const carsInDriveMode: { id: number; time: number }[] = [];
+
+  // Loop through local storage keys
+  for (let i = 0; i < localStorage.length; i += 1) {
+    const key = localStorage.key(i);
+    if (key && key.endsWith('-driveMode')) {
+      const id = parseInt(key.split('-')[0], 10); // Convert id to number
+      const driveMode = localStorage.getItem(key) === 'true';
+      const timeInSecondsKey = `${id}-timeInSeconds`;
+      const time = parseFloat(localStorage.getItem(timeInSecondsKey) || '0');
+
+      if (driveMode) {
+        carsInDriveMode.push({ id, time });
+      }
+    }
+  }
+
+  return carsInDriveMode;
+}
