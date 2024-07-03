d2e340db2b0d18baa89a749eb8d8f67bb3d4a371
Roman Jarmukhametov <roman.jarmukhametov@gmail.com>
2024-07-03 07:15:57 +0500

    feat: add toast duration

--- src/components/custom/Toast.tsx
+++ src/components/custom/Toast.tsx
@@ -1,11 +1,11 @@
 import { useEffect } from 'react';
 import ToastProps from '@/types/ToastProps';
 
-function Toast({ message, onClose }: ToastProps) {
+function Toast({ message, onClose, duration = 3000 }: ToastProps) {
   useEffect(() => {
-    const timer = setTimeout(onClose, 3000); // Hide toast after 3 seconds
+    const timer = setTimeout(onClose, duration); // Hide toast after 3 seconds
     return () => clearTimeout(timer);
-  }, [onClose]);
+  }, [onClose, duration]);
 
   return (
     <div className="fixed bottom-20 right-5 bg-gray-800 text-white p-2 rounded">
--- src/types/ToastProps.ts
+++ src/types/ToastProps.ts
@@ -1,5 +1,7 @@
+/* ToastProps is a type that is used to pass the message, duration, and onClose to the Toast component */
 interface ToastProps {
   message: string;
+  duration?: number;
   onClose: () => void;
 }
 
